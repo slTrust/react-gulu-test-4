505f28ef086023f45926b322640e6d16d7d52ada
huangjx <trustfor@sina.cn>
2019-06-24 13:38:05 +0800

    添加 onClick 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,11 +3,12 @@ import './importIcons';
 import './icon.scss';
 interface IconProps {
   name: string;
+  onClick: ()=> void
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="fui-icon">
+    <svg className="fui-icon" onClick={props.onClick}>
         <use xlinkHref={`#${props.name}`}/>
     </svg>
   );
