cac4a89fcffb56d2df6d0dfd9b24d35d8e3a1c7b
huangjiaxi <trustfor@sina.cn>
2019-08-14 21:52:39 +0800

    删除 layout.tsx 里的 let

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -12,11 +12,22 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
 
 const Layout: React.FunctionComponent<Props> = (props) => {
     const {className, ...rest} = props;
+
+    const children = props.children as Array<ReactElement>
+    const hasAside = children.length && 
+        children.reduce((result,node) => result || node.type === Aside,false);
+    return (
+        <div className={sc('',{extra:[className, hasAside && 'hasAside'].join(' ')})} {...rest}>
+            {props.children}
+        </div>
+    );
+
     // 报错 因为它不一定是数组
     // if(props.children.length){}
 
     // 只能通过断言 来绕过检查  
     // 但是这样就导致 容易写出垃圾代码 ，只是让你写的时候思考 该不该这样写
+    /*
     let hasAside = false;
     if((props.children as Array<ReactElement>).length){
         (props.children as Array<ReactElement>).map(node => {
@@ -32,6 +43,23 @@ const Layout: React.FunctionComponent<Props> = (props) => {
             {props.children}
         </div>
     )
+    */
+
+   
+   /*
+   // 此时还是不行  hasAside 不能在 return 里用
+   if((props.children as Array<ReactElement>).length){
+        const hasAside =(props.children as Array<ReactElement>)
+            .reduce((result,node) => result || node.type === Aside,false);
+   }
+   return (
+       <div className={sc('',{extra:[className, hasAside && 'hasAside'].join(' ')})} {...rest}>
+           {props.children}
+       </div>
+   )
+   */
+
+   
 }
 
 export default Layout;
