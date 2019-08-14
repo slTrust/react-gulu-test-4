5ba561b7f2925e6b586950038441fb822216540e
huangjiaxi <trustfor@sina.cn>
2019-08-14 21:39:25 +0800

    检测 Layout 的 children 的 type

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import {scopedClassMaker} from '../classes';
-import './layout.scss'
+import './layout.scss';
+import Aside from './aside';
 
 const sc = scopedClassMaker('fui-layout')
 
 interface Props extends React.HTMLAttributes<HTMLElement>{
+    // 限制我们的组件的 children 不能是一个字符串，必须是一个元素
+    children: ReactElement | Array<ReactElement>
 }
 
 const Layout: React.FunctionComponent<Props> = (props) => {
     const {className, ...rest} = props;
+    // 报错 因为它不一定是数组
+    // if(props.children.length){}
+
+    // 只能通过断言 来绕过检查  
+    // 但是这样就导致 容易写出垃圾代码 ，只是让你写的时候思考 该不该这样写
+    let hasAside = false;
+    if((props.children as Array<ReactElement>).length){
+        (props.children as Array<ReactElement>).map(node => {
+            console.log(node);
+            // 一旦发现有 aside 就附加一个类 hasAside
+            if(node.type === Aside){
+                hasAside = true;
+            }
+        })
+    }
     return (
-        <div className={sc('',{extra:className})} {...rest}>
+        <div className={sc('',{extra:[className, hasAside && 'hasAside'].join(' ')})} {...rest}>
             {props.children}
         </div>
     )
